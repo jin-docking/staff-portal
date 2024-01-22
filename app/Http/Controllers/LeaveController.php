fafdecbfe7131b9f14458f742d9ba9800c27d395
Athira C <dosathirac@gmail.com>
2024-01-22 18:22:03 +0530

    leave status updated

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\leave;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreleaveRequest;
 use App\Http\Requests\UpdateleaveRequest;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+
 
 class LeaveController extends Controller
 {
@@ -14,23 +17,41 @@ class LeaveController extends Controller
      */
     public function index()
     {
-        //
-    }
+        // Add the following code to the controller file       
+        $leaves = Leave::all();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+        return response()->json($leaves);
+    }   
+
+   
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $user = Auth::user();
+
+        $request->validate([
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+            'description' => 'required|string',
+        ]);
+
+      
+
+        $leave =Leave::create([
+            'user_id' => $user->id,
+            'approval_status' => 'pending',
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
+            'description' => $request->description,
+        ]);
+
+        // $user->leaves()->save($leave);
+        
+        return response()->json(['message' => 'Leave applied successfully', 'data' => $leave]);
+        
     }
 
     /**
@@ -52,7 +73,7 @@ class LeaveController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request)
+    public function update(Request $request, leave $leave)
     {
         //
     }
@@ -60,8 +81,9 @@ class LeaveController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy()
+    public function destroy(leave $leave)
     {
         //
     }
 }
+
