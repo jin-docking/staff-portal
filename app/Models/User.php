b2433bd5ab72242ff2d141198c2691d6700707f5
dosmidhunbabu <dosmidhunb@gmail.com>
2024-08-28 14:59:15 +0530

    created a new Notification file for password reset

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Notifications\ResetPasswordNotification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -84,5 +86,10 @@ class User extends Authenticatable
     public function loginLogs()
     {
         return $this->hasMany(LoginLog::class);
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
     }
 }
